5a0c8359a76822297729a894850cda71a52c4794
Collin Johnson <collin.reed.johnson@gmail.com>
2019-04-07 12:32:57 -0400

    react requests from testDB.js in api to confirm through connection. Changed landing page.

--- client/src/App.js
+++ client/src/App.js
@@ -5,25 +5,39 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { apiResponse: "" };
+    this.state = { 
+      apiResponse: "",
+      dbResponse: "" 
+    };
 }
+
 callAPI() {
     fetch("http://localhost:9000/testAPI")
         .then(res => res.text())
         .then(res => this.setState({ apiResponse: res }));
 }
+callDB() {
+  fetch("http://localhost:9000/testDB")
+      .then(res => res.text())
+      .then(res => this.setState({ dbResponse: res }))
+      .catch(err => err);
+}
 componentDidMount() {
     this.callAPI();
+    this.callDB();
 }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="React-Express">Express React Boilerplate</h1>
+          <h1 className="React-Express">Dockerized MERN Boilerplate</h1>
         </header>
         <p className="App-intro">
           {this.state.apiResponse}
+        </p>
+        <p className="App-intro">
+          {this.state.dbResponse}
         </p>
       </div>
     );
